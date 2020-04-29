e0011ca49eb942949f244d550678a5da98b08a28
zimnykisiel <62248618+zimnykisiel@users.noreply.github.com>
2020-04-29 12:31:47 +0200

    where id=1 or id=3

--- PHP3/34.php
+++ PHP3/34.php
@@ -15,17 +15,12 @@
     try {
         $db = new PDO("mysql:host=$host;dbname=$database", $user, $password, 
               array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-    
-        $query = 'SELECT marka, model, pojemnosc FROM samochody';
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $results = $statement->fetchAll();
-        $statement->closeCursor();      
-    
-        foreach($results as $r){
-                echo "Marka: ".$r['marka'] . '<br />';
-                echo "Model: ".$r['model'] . '<br />';
-                echo "Pojemnosc: ".$r['pojemnosc'] . '<br />';
+        $sql = 'SELECT marka, model, pojemnosc FROM samochody where id=1 or id=3';
+        $stmt = $db -> query($sql) -> fetchAll();
+        foreach($stmt as $row){
+            echo "Marka: ".$row['marka']."</br>";
+            echo "Model: ".$row['model']."</br>";
+            echo "Pojemnosc: ".$row['pojemnosc']."</br>";
         }
     
     } catch (PDOException $e) {
